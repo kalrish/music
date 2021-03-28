03347913a0be7aa396b51cffe6b6510bcaf9a2aa
David Joaquín Shourabi Porcel <david@djsp.eu>
2021-03-28 01:55:56 +0100

    Add script to import sources from HDtracks

---
+++ utils/import/hdtracks.sh
@@ -0,0 +1,81 @@
+set -e
+
+function get_flac_tag
+{
+	local flac_filename="${1}"
+	local tag_name="${2}"
+
+	metaflac \
+		"--show-tag=${tag_name}" \
+		-- \
+		"${flac_filename}" | sed -e "s/^${tag_name}=//"
+}
+
+source_dir="${1:?source directory not specified}"
+target_dir="${2:?target directory not specified}"
+artist_dir="${3:?artist directory not specified}"
+album_dir="${4:?album directory not specified}"
+
+cd \
+	"${source_dir}" \
+	#
+
+album_path="${target_dir}/${artist_dir}/${album_dir}"
+
+mkdir \
+	--verbose \
+	--parents \
+	-- \
+	"${album_path}" \
+	#
+
+mustup_manifest_yaml_path="${album_path}/mustup-manifest.yaml"
+
+{
+	echo '---'
+
+	for flac_filename in *.flac
+	do
+		wave_filename="${flac_filename}"
+		wave_filename="${wave_filename/ - /-}"
+		wave_filename="${wave_filename// /_}"
+		wave_filename="${wave_filename//\'/}"
+		wave_filename="${wave_filename,,}"
+		wave_filename="${wave_filename%.flac}.wave"
+
+		wave_path="${album_path}/${wave_filename}"
+
+		flac \
+			--decode \
+			"--output-name=${wave_path}" \
+			-- \
+			"${flac_filename}" \
+			#
+
+		title="$(
+			get_flac_tag \
+				"${flac_filename}" \
+				TITLE \
+				#
+		)"
+
+		echo
+		echo "${wave_filename}:"
+		echo '   metadata:'
+		echo '      tags:'
+		echo '         common:'
+		echo "            title: ${title}"
+	done
+} > "${mustup_manifest_yaml_path}"
+
+# Check that the generated `mustup-manifest.yaml` is valid YAML
+yamllint \
+	-- \
+	"${mustup_manifest_yaml_path}" \
+	#
+
+metaflac \
+	--export-tags-to - \
+	-- \
+	*.flac \
+	#
