0248b6c94cff0c94a49718122b4edb3f9859a549
David Joaquín Shourabi Porcel <david@djsp.eu>
2021-03-28 01:57:25 +0100

    Add script to export profiles

---
+++ utils/export.sh
@@ -0,0 +1,25 @@
+# sh -- export.sh home /run/media/kalrish/music/music
+# sh -- export.sh honor10 honor10:/sdcard/Music
+
+profile="${1:?profile not specified}"
+target_dir="${2:?target directory not specified}"
+
+#	--dry-run \
+rsync \
+	--verbose \
+	--recursive \
+	--exclude .gitignore \
+	--exclude tup.config \
+	--exclude '*.jpeg' \
+	--exclude '*.png' \
+	--exclude '*.vc' \
+	--times \
+	--delete-before \
+	--inplace \
+	--no-perms \
+	--progress \
+	--rsh=ssh \
+	-- \
+	"output-${profile}/" \
+	"${target_dir}/vibes" \
+	#
